322700b3e2f2574fb1801b071ff34370f6a73cde
herbart <burnedcard77@gmail.com>
2024-10-31 11:46:07 -0700

    dynamic resolution, rainbow mode, opacity changing functionality

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 
 const container = document.querySelector("#container");
 const button = document.querySelector(`#btn`);
+const userColor = document.querySelector('#colorBtn');
+let isRainbow = false;
+let isBlack = true;
+
+userColor.addEventListener('click',()=>{
+    if(isRainbow === false){
+        isRainbow = true;
+        isBlack = false;
+        userColor.innerHTML = "make black";
+    }
+    else if (isRainbow === true){
+        isRainbow = false;
+        isBlack = true;
+        userColor.innerHTML = "make rainbow";
+    }
+})
 
 // init
 for (let i = 0; i < 16; i++){
@@ -17,10 +33,38 @@ for (let i = 0; i < 16; i++){
     container.appendChild(rowDiv);
 }
 
+
+function randomRGB(){
+    let value = Math.floor(Math.random() * (255 - 0 + 1));
+    return value;
+}
+
 document.querySelector("#container").addEventListener("mouseover", (e) => {
     if(e.target.matches('.colDiv')){
         const hoverDiv = e.target;
-        hoverDiv.style.backgroundColor = 'black';
+        if(isRainbow === true){
+            hoverDiv.style.backgroundColor = `rgb(${randomRGB()},${randomRGB()},${randomRGB()})`; 
+        }
+        else if(isBlack === true){
+            hoverDiv.style.backgroundColor = 'black';
+        }
+
+        const computedStyle = window.getComputedStyle(hoverDiv);
+
+        const opacity = computedStyle.opacity;
+        console.log(opacity);
+        console.log(computedStyle.opacity);
+        
+        if(hoverDiv.matches('.marked')){
+           hoverDiv.style.opacity = `${opacity - 0.1}` 
+        }
+
+        if(!e.target.matches('.marked')){
+            e.target.classList.toggle('marked');
+        }
+        // to do
+        // 1. Make background tiles after marked black to darken instead of lighten
+        // 2. After marked and in rainbow mode do not change the color again
     }
 })
 
@@ -54,9 +98,4 @@ button.addEventListener("click", ()=>{
     container.replaceChildren();
     // repaint board
     newRes(input);
-
-
-
-
-
 })
