2183a5f5ef678fb183ece97aa4bdd996e571e85c
herbart <burnedcard77@gmail.com>
2024-10-30 18:09:47 -0700

    added sketch functionality and resolution changing

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const container = document.querySelector("#container");
+const button = document.querySelector(`#btn`);
 
+// init
 for (let i = 0; i < 16; i++){
     let rowDiv = document.createElement("div");
     rowDiv.classList.toggle(`r${i}`);
@@ -10,8 +12,51 @@ for (let i = 0; i < 16; i++){
         colDiv.classList.toggle(`r${i}c${j}`);
         colDiv.classList.toggle(`colDiv`);
         rowDiv.appendChild(colDiv);
-        console.log(`row: ${i}, col: ${j}`);
+        // console.log(`row: ${i}, col: ${j}`);
     }
     container.appendChild(rowDiv);
 }
 
+document.querySelector("#container").addEventListener("mouseover", (e) => {
+    if(e.target.matches('.colDiv')){
+        const hoverDiv = e.target;
+        hoverDiv.style.backgroundColor = 'black';
+    }
+})
+
+function newRes(value){
+    for (let i = 0; i < value; i++){
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.toggle(`r${i}`);
+        rowDiv.classList.toggle(`rowDiv`);
+        for(let j = 0; j < value; j++){
+            let colDiv = document.createElement("div");
+            colDiv.classList.toggle(`r${i}c${j}`);
+            colDiv.classList.toggle(`colDiv`);
+            rowDiv.appendChild(colDiv);
+            // console.log(`row: ${i}, col: ${j}`);
+        }
+        container.appendChild(rowDiv);
+    }
+}
+
+button.addEventListener("click", ()=>{
+    let input = Number(prompt("Choose a new value MAX: 100"));
+    while(true){
+        if(input > 0 && input <= 100){
+            break;
+        }
+        input = Number(prompt("Value must be 1 - 100"));
+    }
+    console.log(input);
+
+    // simple way to delete all its children
+    container.replaceChildren();
+    // repaint board
+    newRes(input);
+
+
+
+
+
+})
